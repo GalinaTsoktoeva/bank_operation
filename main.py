c9bc53de32a3ae3b7fe784205183de6b42991f73
gtsoktoeva001 <galina-tsoktoeva@mail.ru>
2023-03-22 01:28:56 +0300

    added main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from utils.download_json import download_data
+def main():
+    filename = "operations.json"
+    download_data(filename)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/

2f62090d5677b5fbfb4936e53ca55c098499e9bc
gtsoktoeva001 <galina-tsoktoeva@mail.ru>
2023-03-27 19:22:02 +0300

    added main.py

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
-from utils.download_json import download_data
+from utils.util import download_data, refactor_string
+from datetime import datetime
+
 def main():
-    filename = "operations.json"
-    download_data(filename)
+    count_str = 0
+    result = ""
+    filename = "utils/operations.json"
+    data = download_data(filename)
+
+    for item in range(len(data)):
+
+        try:
+            str = refactor_string(data[item])
+            if str:
+                print()
+                str = ""
+
+            if count_str == 5:
+                break
+            count_str += 1
+        except:
+            pass
+
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
